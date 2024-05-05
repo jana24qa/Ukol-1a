d68b0f6ac70ed1728b91f9bb90b74c8b41244c19
Jana Radova <jana.penickova@gmail.com>
2024-05-05 21:58:36 +0200

    Ukol-1a

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,34 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+import java.math.BigDecimal;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String [] args) {
+        System.out.println("Hello world!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        String uzivatel = "Karel";
+        System.out.println(uzivatel);
+
+        int pocetLekci = 10;
+        System.out.println(pocetLekci);
+
+        double cena = 10.0;
+        System.out.println(cena);
+
+        BigDecimal totalPriceBig = BigDecimal.ZERO;
+        for (int i = 0; i < 10; i++) {
+            totalPriceBig = totalPriceBig.add(BigDecimal.valueOf(0.1));
         }
+        System.out.println(totalPriceBig);
+
+        BigDecimal cena2 = BigDecimal.ZERO;
+        cena2 = cena2.add(BigDecimal.valueOf(0.1));
+        System.out.println(cena2);
+
+        String vykonMotoru = "120";
+        System.out.println("Výkon Motoru je: "+vykonMotoru+" kW.");
+
+        int velikostKosile = 37;
+        System.out.println("Velikost košile je: "+ velikostKosile);
+
+        System.out.println("Metoda skoro bez chybičky!");
     }
 }
